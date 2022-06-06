a1063cf1f833285804c21080801f242fb003ae1e
Nurlibek <nurikmurat98@gmail.com>
2022-06-06 11:53:20 +0600

    fix

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-getRandomIntInclusive(5, 15);
+getRandomIntInclusive(5, 15)
 
 function checkLength(string, maxLength) {
-  return string.length > maxLength ? false : true;
+  if (maxLength > string.length) {
+    return true
+  } else {
+    return false
+  }
 }
 
-checkLength("Apple", 4);
+checkLength('Apple', 4)
