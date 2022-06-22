0295854c3e47a20ee69dded53c6e1c238f14ad25
Nurlibek <nurikmurat98@gmail.com>
2022-06-22 12:32:05 +0600

    7.16. Отрисуй меня полностью (часть 2)

--- js/render-picture.js
+++ js/render-picture.js
@@ -1,18 +1,24 @@
 import { createRandomPosts } from './data.js';
-
-const pictures = document.querySelector('.pictures');
+import { showPicture } from './gallery.js';
 
 const randonPicturesTemplate = document.querySelector('#picture').content;
-
+const picture = document.querySelector('.pictures');
 const randonPictures = createRandomPosts();
 const randonPicturesFragment = document.createDocumentFragment();
 
-randonPictures.forEach(({ url, likes, comments }) => {
+randonPictures.forEach(({ url, likes, description, comments }) => {
   const pictureElement = randonPicturesTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent =
     comments.length;
+  pictureElement.querySelector('.picture').addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showPicture(url, likes, description, comments);
+  });
   randonPicturesFragment.append(pictureElement);
 });
-pictures.append(randonPicturesFragment);
+
+picture.append(randonPicturesFragment);
+
+export { randonPictures };
